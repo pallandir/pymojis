dc7ed90bb4b239f7d04b2d9febad708484752ac4
pallandir <pallandir2@gmail.com>
2025-07-13 03:33:03 +0800

    Update domain repository

--- src/domain/repositories/repository.py
+++ src/domain/repositories/repository.py
@@ -1,23 +1,26 @@
 from abc import ABC, abstractmethod
+from typing import Literal
 
 from src.domain.entities.emojis import Categories, Emoji
 
 
 class PymojisRepository(ABC):
     @abstractmethod
-    def get_all(self) -> list[Emoji]:
+    def get_all(self, exclude: Literal["complex"] | list[Categories]) -> list[Emoji]:
         pass
 
     @abstractmethod
-    def get_random_emoji(self):
+    def get_random_emojis(
+        self, category: Categories | None = None, length: int = 1
+    ) -> list[Emoji]:
         pass
 
     @abstractmethod
-    def get_by_code(self, code: str) -> str:
+    def get_by_code(self, code: str) -> str | None:
         pass
 
     @abstractmethod
-    def get_by_name(self, name: str) -> str:
+    def get_by_name(self, name: str) -> str | None:
         pass
 
     @abstractmethod
@@ -25,5 +28,5 @@ class PymojisRepository(ABC):
         pass
 
     @abstractmethod
-    def validate_emoji(self, code: str) -> str:
+    def validate_emoji(self, code: str) -> list[str]:
         pass
