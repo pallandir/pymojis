15ed03eda853011975eb907333d841034a2a4a39
pallandir <pallandir2@gmail.com>
2025-07-15 00:57:20 +0800

    Add get_by_category test

--- tests/infrastructure/test_emoji_repository.py
+++ tests/infrastructure/test_emoji_repository.py
@@ -14,3 +14,9 @@ def test_emoji_repository_get_by_code(repository):
 def test_emoji_repository_get_by_name(repository):
     result = repository.get_by_name("grinning face")
     assert result == "😀"
+
+
+def test_emoji_repository_bet_by_category(repository):
+    result = repository.get_by_category("Smileys & Emotion")
+    assert isinstance(result, list)
+    assert len(result) > 0
