22450e923f7065886f10818ea04de06d6c262460
pallandir <pallandir2@gmail.com>
2025-07-15 00:18:01 +0800

    Add repository get_all test

---
+++ tests/infrastructure/test_emoji_repository.py
@@ -0,0 +1,10 @@
+from src.infrastructure.emojis_repository import EmojisRepository
+from src.domain.entities.emojis import Emoji
+
+
+def test_emoji_repository_get_all():
+    repository = EmojisRepository(
+        data_file_path="tests/infrastructure/data/mock_emojis_data.json"
+    )
+    result = repository.get_all(None)
+    assert isinstance(result[0], Emoji)
